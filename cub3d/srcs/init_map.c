fb904f1acd449d28a3c7813c0340e936f83772d1
Gereltuya <gereltuya.altangerel@gmail.com>
2024-03-28 23:48:51 +0100

    found error in my GNL, leak when EOF is last line

--- cub3d/srcs/init_map.c
+++ cub3d/srcs/init_map.c
@@ -50,7 +50,7 @@ char	**ft_getmap(char **tmp, int size)
 	map = (char **)malloc((size + 1) * sizeof(char *));
 	if (!map)
 		return (NULL);
-	while (tmp[i] && ft_str_is_printable(tmp[i]) && i < size)
+	while (tmp[i] && i < size)
 	{
 		map[i] = ft_strdup(tmp[i]);
 		if (!map[i])
@@ -64,11 +64,32 @@ char	**ft_getmap(char **tmp, int size)
 	return (map);
 }
 
+int	ft_aftermap(char **map, char **tmp)
+{
+	int		i;
+	size_t	j;
+
+	i = 0;
+	while (map[i])
+	{
+		if (!ft_str_is_printable(map[i]))
+			return (0);
+		j = 0;
+		while (map[i][j] && ft_iswhitespace(map[i][j]))
+			j++;
+		if (ft_strlen(map[i]) == j)
+			return (0);
+		i++;
+	}
+	if (tmp[i])
+		return (0);
+	return (1);
+}
+
 int	ft_initmap(t_cub3d *cub3d, char **tmp)
 {
 	int	i;
 	int	size;
-	int	x;
 
 	i = 0;
 	size = 0;
@@ -79,12 +100,9 @@ int	ft_initmap(t_cub3d *cub3d, char **tmp)
 	cub3d->map = ft_getmap(&tmp[i], size);
 	if (!cub3d->map)
 		return (0);
-	if (!ft_check_map(cub3d->map, cub3d))
+	if (!ft_aftermap(cub3d->map, &tmp[i]))
 		return (0);
-	x = 0;
-	while (cub3d->map[x])
-		x++;
-	if (tmp[i + x])
+	if (!ft_check_map(cub3d->map, cub3d))
 		return (0);
 	return (1);
 }
